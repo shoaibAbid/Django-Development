0c0feae84dc0944dab88f2db65019ec7500c9345
shoaibAbid <73983203+shoaibAbid@users.noreply.github.com>
2021-08-04 19:37:47 +0500

    Ecommerce Website

---
+++ website/store/admin.py
@@ -0,0 +1,16 @@
+from store.models.category import Category
+from django.contrib import admin
+from .models.product import Product
+from .models.customer import Customer
+from .models.orders import Order
+
+# Register your models here.
+class AdminProduct(admin.ModelAdmin):
+    list_display=['name','price','category']
+admin.site.register(Product,AdminProduct)
+
+class AdminCategory(admin.ModelAdmin):
+    list_display=['name']
+admin.site.register(Category,AdminCategory)
+admin.site.register(Customer)
+admin.site.register(Order)
